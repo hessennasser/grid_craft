bd7829ff3c6268ce1e73211442fdcf2009b0031e
Hessen Nasser <hessenaloosh@gmail.com>
2024-09-22 12:17:00 +0300

    update header style

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -11,7 +11,7 @@ export const Header: React.FC = () => {
 
     return (
         <header className="fixed w-full top-0 left-0 h-fit z-50 px-2 md:px-6 py-2">
-            <div className="border-gray-200 dark:border-gray-700/50 bg-white dark:bg-gray-900/50 p-4 shadow-md container mx-auto flex justify-between items-center rounded-lg backdrop-blur-md">
+            <div className="border-gray-200 dark:border-gray-700/50 bg-white/50 dark:bg-gray-900/50 p-4 shadow-md container mx-auto flex justify-between items-center rounded-lg backdrop-blur-md">
                 <h1 className="text-2xl md:text-3xl font-bold text-gray-900 dark:text-gray-100">
                     <Link href="/">GridCraft</Link>
                 </h1>
